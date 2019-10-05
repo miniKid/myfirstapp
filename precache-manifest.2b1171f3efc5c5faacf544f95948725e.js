fc99101b3081647f1e791a7c2819490a35f90e68
LeonChi <minyue.chi@outlook.com>
2019-10-05 22:50:52 +0800

    Updates

---
+++ precache-manifest.2b1171f3efc5c5faacf544f95948725e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305007bc84dc45a1a2bf437f42a214f4",
+    "url": "/myfirstapp/index.html"
+  },
+  {
+    "revision": "39d63f6c4bb11a79adbc",
+    "url": "/myfirstapp/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "8864efc1998794c360f2",
+    "url": "/myfirstapp/static/css/main.ef1e49ff.chunk.css"
+  },
+  {
+    "revision": "39d63f6c4bb11a79adbc",
+    "url": "/myfirstapp/static/js/2.9a3ae6a2.chunk.js"
+  },
+  {
+    "revision": "8864efc1998794c360f2",
+    "url": "/myfirstapp/static/js/main.e4c7be90.chunk.js"
+  },
+  {
+    "revision": "69a65094dd5ae91af55a",
+    "url": "/myfirstapp/static/js/runtime~main.80a2e2e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
